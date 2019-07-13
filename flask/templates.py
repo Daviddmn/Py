894e2e4184eadb38ab25f46e55d801f757cd62c0
David <51552786+Daviddmn@users.noreply.github.com>
2019-07-14 01:11:06 +0300

    Update templates.py

--- flask/templates.py
+++ flask/templates.py
@@ -14,3 +14,29 @@ if __name__ == '__main__':
 
 # ფლასკი შეეცდება იპოვოს HTML ფაილი თიმფლეითის ფოლდერში, იმ ფოლდერის შიგნით სადაც პითონის სკრიპტია გაშვებული,
 # Application folder > flask.py > templates > index.html
+
+# # # # # # # # # # # # # #
+# web templating system გამოიყენება იმისთვის რომ HTML კოდში ჩავდოთ დინამიური მონაცემთა ცვლადი(var)
+# პითონის ცვლადის HTML ში გასაშვებად საჭიროა {{ }} - ორმაგი ბრეკეტი და სიგნით განვსაძღვრავთ ცვლადის სახელს.
+
+#HTML კოდი (hello.html)
+<html>
+   <body>
+   
+      <h1>Hello {{ test }}!</h1>
+      
+   </body>
+</html>
+
+# Flask კოდი.
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('hello.html',test='aq ganvsazgvravt cvlads, aseve strings.')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
+# სერვერის გაშვების შემდეგ , დიდი ფონტით დაბრუნდება წარწერა   aq ganvsazgvravt cvlads, aseve strings. 

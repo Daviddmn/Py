b5d37c0d247166ef84eb572862f002989fe79279
David <51552786+Daviddmn@users.noreply.github.com>
2019-07-13 19:41:22 +0300

    Update templates.py

--- flask/templates.py
+++ flask/templates.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template(‘hello.html’)
+   return render_template('hello.html')
 
 if __name__ == '__main__':
    app.run(debug = True)

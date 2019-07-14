3a1c1070e6fe1d115c4115feca5214c8c1d026cb
David <51552786+Daviddmn@users.noreply.github.com>
2019-07-14 03:09:19 +0300

    Update templates.py

--- flask/templates.py
+++ flask/templates.py
@@ -40,3 +40,37 @@ if __name__ == '__main__':
    app.run(debug = True)
 
 # სერვერის გაშვების შემდეგ , დიდი ფონტით დაბრუნდება წარწერა   aq ganvsazgvravt cvlads, aseve strings. 
+
+
+
+
+# # # #
+# {% %} - აქ გაეშვება statements-ები (for,if,else,endif...)  
+#html
+<html>
+   <body>
+      {% if marks>50 %}
+         <h1> Your result is success!</h1>
+      {% else %}
+         <h1>Your result is fail</h1>
+      {% endif %}
+   </body>
+</html>
+
+#flask # # # # ## # # # # # # # # # #
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/hello/<int:score>')
+def hello_name(score):
+   return render_template('hello.html', marks = score)
+
+if __name__ == '__main__':
+   app.run(debug = True)
+   
+# თუ URL ში შეყვანილი იქნება რიცხვი 50 ზე მეტი გამოიტანს 'შედეგი წარმატებულია' თუ ნაკლებია 'შედეგი წარუმატებელი'  
+# ბოლოში statement ის დასასრულებლად იწერება {% endif %}  
+# {{ ბრეკეტებს შორის გამოტოვება არის რეკომენდირებული. }}
+
+
+
